6b8a9943046dc2b71c4a1e8d6092afc78f77bc2f
knowledgeAlan <1299076979@qq.com>
2024-07-02 18:32:19 +0800

    read and write contract on etherum

--- src/app/custom-wallet/custom-wallet.tsx
+++ src/app/custom-wallet/custom-wallet.tsx
@@ -1,9 +1,10 @@
 "use client"
-import React ,{useEffect} from "react";
+import React ,{useEffect, useState} from "react";
 import { useWeb3Modal } from "@web3modal/wagmi/react";
-import {useAccount,useDisconnect,useBalance} from "wagmi";
+import {useAccount,useDisconnect,useBalance,useReadContract,useWriteContract} from "wagmi";
 import { format } from "path";
 import BigNumber from "bignumber.js";
+import wagmiLockABI from "@/app/abi/wagmiLockABI.json"
 
 export default function CustomWallet() {
 
@@ -11,15 +12,25 @@ export default function CustomWallet() {
     const { open} = useWeb3Modal();
     const {isConnected,address } = useAccount();
     const {disconnect} = useDisconnect();
+    
 
+    const { writeContract } = useWriteContract()
+ 
    
    const{data}= useBalance({
         address:address,
     });
 
+    const result = useReadContract({
+        abi:wagmiLockABI,
+        address: '0x7f21D58debc126c9b6635BdAaac62a0032443e13',
+        functionName: 'getWagmiReadParameter',
+      });
+
     if(isConnected){
         let value:string=data?.value.toString() ? data.value.toString() : "";
         let decimal:number = data?.decimals ? data?.decimals : 0;
+        let readData:string ="";
      
 
         let numberPow =new BigNumber(10).pow(decimal);
@@ -28,7 +39,38 @@ export default function CustomWallet() {
         let balance = Number(value)/Number(numberPow.toString()) ;
         console.log("balance====="+balance);
 
-        return <button onClick={ ()=> disconnect() }>DisConnect {address}=symbol=={data?.symbol}==balance=={balance.toFixed(3)}==</button>;
+        console.log("readContractData==isSuccess===",result.isSuccess);
+        if(result.isSuccess){
+            
+            console.log("readContractData==data===",result.data);
+            readData = result.data ?  result.data.toString() :"123";
+            console.log("readData==",readData)
+             
+        }
+        
+        
+        const writeContractTest = ()=>{
+
+
+            console.log("writeContract==");
+
+            
+
+             writeContract({ 
+                abi:wagmiLockABI,
+                address: '0x7f21D58debc126c9b6635BdAaac62a0032443e13',
+                functionName: 'setWagmiReadParameter',
+                args: [
+                  12n,
+                ],
+             })
+            
+        }
+
+        return <>
+        <button onClick={ ()=> disconnect() }>DisConnect {address}=symbol=={data?.symbol}==balance=={balance.toFixed(3)}=read=data=={readData}=</button>
+        <button onClick={ ()=> writeContractTest() }>write</button>
+        </> ;
     }
 
    
@@ -40,6 +82,9 @@ export default function CustomWallet() {
 
       
     }
+
+
+   
    // useEffect(() => {
 
         // if(isConnected){
